17b2848216fa3c705bb43ee7e423f5a673c6e924
jgaleano2018 <32717587+jgaleano2018@users.noreply.github.com>
2023-01-10 17:12:37 -0500

    feature: Create project with minimalAPI architecture

---
+++ project-minimal-api/TodoDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace project_minimal_api
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+       
+    }
+}
